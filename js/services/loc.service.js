87109333da1449f3b096aa974b294013b1fd7c07
T0mer007 <tomerbenaim1@gmail.com>
2023-05-03 16:55:19 +0300

    added async-storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,14 +1,13 @@
 
-import { storage } from './storage.service.js'
+import { storage } from './services/async-storage.service.js'
 
 export const locService = {
     getLocs
 }
-
  
 const STORAGE_KEY = 'locationsDB'
 
-const locs = storage.loadFromStorage(STORAGE_KEY) || [
+const locs = storage.query(STORAGE_KEY) || [
     { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
